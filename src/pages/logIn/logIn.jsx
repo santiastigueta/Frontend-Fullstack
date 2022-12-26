c5034d90b22a5419a91c5b9a3a20ab916a362cee
santi <santiasti0906@gmail.com>
2022-12-26 16:25:14 -0300

    actualización 24/12

--- src/pages/logIn/logIn.jsx
+++ src/pages/logIn/logIn.jsx
@@ -1,48 +1,67 @@
-import React from 'react'
-import AppBarBis from '../../components/NavBarBis/NavBarBis';
-import TextField from '@mui/material/TextField';
-import { Button } from '@mui/material';
-import { useState } from 'react';
-import { LOGIN_USUARIO } from '../../graphql/resolvers/user.resolver';
-import { useQuery } from '@apollo/client';
+import React from "react";
+import AppBarBis from "../../components/NavBarBis/NavBarBis";
+import TextField from "@mui/material/TextField";
+import { Button } from "@mui/material";
+import { useState } from "react";
+import { LOGIN_USUARIO } from "../../graphql/resolvers/user.resolver";
+import { useLazyQuery } from "@apollo/client";
+
 
 const Login = () => {
   const [formState, setFormState] = useState({
     email: "",
     password: "",
-  });
-  const {error, loading, data} = useQuery(LOGIN_USUARIO, {
+  }); 
+
+  const [loginUsuario, { error, loading, data }] = useLazyQuery(LOGIN_USUARIO, {
     variables: {
       email: formState.email,
-      password: formState.password
-    }
+      password: formState.password,
+    } 
   });
   if (loading) return <p> Loading... </p>;
   if (error) return <p> Error: </p>;
-
-  return (
+   return (
     <>
       <AppBarBis></AppBarBis>
       <h1>¿Ya tenés una cuenta? Inicia Sesión</h1>
       <form
-        onSubmit={(e)=>{
+        onSubmit={(e) => {
           e.preventDefault();
-          // funcion del resolver
-          console.log('usuario encontrado: ', e)
-        }}>
+          console.log('user encontrado: ', data.loginUsuario)
+          loginUsuario();
+        }}
+      >
         <TextField
           id="outlined-required"
           label="email"
+          value={formState.email}
+          onChange={(e) => {
+            setFormState({
+              ...formState,
+              email: e.target.value,
+            });
+          }}
+          type="text"
         />
         <TextField
           id="outlined-required"
           label="password"
+          value={formState.password}
+          onChange={(e) => {
+            setFormState({
+              ...formState,
+              password: e.target.value,
+            });
+          }}
+          type="text"
         />
-        <Button type='submit'  variant="contained" color="secondary" >Hecho</Button>
+        <Button type="submit" variant="contained" color="secondary">
+          Hecho
+        </Button>
       </form>
     </>
-    
-  )
-}
+  );
+}; 
 
 export default Login;
--- src/pages/register/register.jsx
+++ src/pages/register/register.jsx
@@ -7,75 +7,75 @@ import { REGISTER_USUARIO } from "../../graphql/resolvers/user.resolver";
 import { Button } from "@mui/material";
 import { Link } from "react-router-dom";
 const Register = () => {
-    const [formState, setFormState] = useState({
-        email: "",
-        username: "", 
-        password: ""
-    });
-    
-    const [registerUsuario] = useMutation(REGISTER_USUARIO, {
-        variables:{
-            email: formState.email,
-            username: formState.username,
-            password: formState.password
-        }
-    })
+  const [formState, setFormState] = useState({
+    email: "",
+    username: "",
+    password: "",
+  });
 
-    const createUser = () => {
-        console.log('usuario creado: ',formState)
-        registerUsuario();
-    }
+  const [registerUsuario] = useMutation(REGISTER_USUARIO, {
+    variables: {
+      email: formState.email,
+      username: formState.username,
+      password: formState.password,
+    },
+  });
+
+  const createUser = () => {
+    
+    console.log("usuario creado: ", formState);
+    registerUsuario();
+  };
 
   return (
     <div className="register">
-        <h1>Registrarse</h1>
+      <h1>Registrarse</h1>
       <form
-        onSubmit={(e)=>{
-            e.preventDefault();
-            createUser();
+        onSubmit={(e) => {
+          e.preventDefault();
+          createUser();
         }}
       >
         <TextField
           id="outlined-required"
           label="username"
           value={formState.username}
-          onChange={(e)=>{
+          onChange={(e) => {
             setFormState({
-                ...formState,
-                username: e.target.value
-            })
+              ...formState,
+              username: e.target.value,
+            });
           }}
         />
         <TextField
           id="outlined-required"
           label="e-mail"
           value={formState.email}
-          onChange={(e)=>{
+          onChange={(e) => {
             setFormState({
-                ...formState,
-                email: e.target.value
-            })
+              ...formState,
+              email: e.target.value,
+            });
           }}
         />
-         <TextField
+        <TextField
           id="outlined-password-input"
           label="Password"
           value={formState.password}
           type="password"
-          onChange={(e)=>{
+          onChange={(e) => {
             setFormState({
-                ...formState,
-                password: e.target.value
-            })
+              ...formState,
+              password: e.target.value,
+            });
           }}
         />
         <Button type="submit" variant="contained" color="info">
-            Hecho
+          Hecho
         </Button>
-        
       </form>
       <Button component={Link} to="/" variant="contained" color="secondary">
-          volver
+        volver
       </Button>
     </div>
   );
